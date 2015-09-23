e456507589360addcf14403c22616bff98e8793e
Kevin Chen <kevin.chen@kchen-mb13r.corp.creditkarma.com>
2015-09-23 00:24:51 -0700

    Adds switch filters

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,26 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Kevin Chen on 9/23/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class SwitchCell: UITableViewCell {
+
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var onSwitch: UISwitch!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
